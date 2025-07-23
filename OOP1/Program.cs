1057584a05ad6ceaf70f1ceb337af382d35a30cc
Fahd <fahdmo7amed76@gmail.com>
2025-07-23 12:22:02 +0300

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Data.SqlTypes;
+
+namespace OOP1
+{
+
+
+    //class 
+    //Struct
+    //Interfacde 
+    //Enum
+
+
+
+    internal class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("V01");
+            #region Struct
+            ////struct : Value Type : STACK  
+
+            ////Employee
+            ////ID , Name ,Salary 
+
+            ////Point 
+
+            //// Point P01;
+            ////Declare For Object From Type 'Point'
+            ////P01: Object 
+            ////allocate Uninitialized 8 Bytes at Stack
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+            ////P01.X = 12;
+            ////P01.Y = 12;
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            ////  P01 = new Point(1,2 );
+            ////new : Used for Select The Constructor
+
+
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+            ////  Console.WriteLine(P01);
+            ////Console.WriteLine();
+
+            ////P01.PrintPoint();
+
+            ////  Console.WriteLine(P01); 
+            ///
+
+            // Point[] points = new Point[10];
+
+
+
+            #endregion
+
+
+        }
+    }
+}
+
